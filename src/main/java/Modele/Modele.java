2ca080372eec474ac750d9271f3e9889ab1d5c13
Mairymhub <Falek.myriam@hotmail.com>
2024-11-25 18:06:10 +0100

    modification
    controleur.java
    modele.java
    connexion.java

--- src/main/java/Modele/Modele.java
+++ src/main/java/Modele/Modele.java
@@ -1,4 +1,4 @@
-package Modele;
+package modele;
 
 import java.util.ArrayList;
 
@@ -7,17 +7,65 @@ import controleur.Entreprise;
 public class Modele {
 	private static Connexion uneConnexion = new Connexion("localhost","agroEco","root","");
 	
+	
+	/****** Gestion des déchets ******/
 	public static void insertDechet (Dechet unDechet) {
-		
 	}
 	public static ArrayList<Dechet> selectAllDechets(){
 		ArrayList<Dechet> lesDechets = new ArrayList<>();
 		return lesDechets;
 	}
-	
 	public static ArrayList<Dechet> selectLikeDechets(String filtre){
+		ArrayList<Dechet> lesDechets = new ArrayList<>();
 		return lesDechets;
 	}
-	public static void deletDechet(int idDechet)
+	public static void deleteDechet(int idDechet) {
+	}
+	public static Dechet selectWhereDechet(int idDechet) {
+		Dechet unDechet = null;
+		return unDechet;
+	}
+	public static void updateDechet(Dechet unDechet) {
+	}
 	
+	
+	/****** Gestion de la consommation d'eau ******/
+	public static void insertConsommationEau (ConsommationEau uneConsommationEau) {
+	}
+	public static ArrayList<ConsommationEau> selectAllConsommationsEau(){
+		ArrayList<ConsommationEau> lesConsommationsEau = new ArrayList<>();
+		return lesConsommationsEau;
+	}
+	public static ArrayList<ConsommationEau> selectLikeConsommationsEau(String filtre){
+		ArrayList<ConsommationEau> lesConsommationsEau = new ArrayList<>();
+		return lesConsommationsEau;
+	}
+	public static void deleteConsommationEau(int idConsommationEau) {
+	}
+	public static ConsommationEau selectWhereConsommationEau(int idConsommationEau) {
+		ConsommationEau uneConsommationEau = null;
+		return uneConsommationEau;
+	}
+	public static void updateConsommationEau(ConsommationEau uneConsommationEau) {
+	}
+	
+	/****** Gestion de l'empreinte carbone ******/
+	public static void insertEmpreinteCarbone (EmpreinteCarbone uneEmpreinteCarbone) {
+	}
+	public static ArrayList<EmpreinteCarbone> selectAllEmpreintesCarbone(){
+		ArrayList<EmpreinteCarbone> lesEmpreintesCarbone = new ArrayList<>();
+		return lesEmpreintesCarbone;
+	}
+	public static ArrayList<EmpreinteCarbone> selectLikeEmpreintesCarbone(String filtre){
+		ArrayList<EmpreinteCarbone> lesEmpreintesCarbone = new ArrayList<>();
+		return lesEmpreintesCarbone;
+	}
+	public static void deleteDechet(int idEmpreinteCarbone) {
+	}
+	public static EmpreinteCarbone selectWhereEmpreinteCarbone(int idEmpreinteCarbone) {
+		EmpreinteCarbone uneEmpreinteCarbone = null;
+		return uneEmpreinteCarbone;
+	}
+	public static void updateEmpreinteCarbone(EmpreinteCarbone uneEmpreinteCarbone) {
+	}
 }
--- src/main/java/controleur/Controleur.java
+++ src/main/java/controleur/Controleur.java
@@ -1,7 +1,75 @@
 package controleur;
 
-import jakarta.servlet.http.HttpServlet;
+import java.util.ArrayList;
 
-public class Controleur extends HttpServlet {
+import modele.Modele;
+
+public class Controleur {
+	
+	/****** Gestion des déchets ******/
+
+	public static void insertDechet (Dechet unDechet) {
+		Modele.insertDechet(unDechet);
+	}
+	public static ArrayList<Dechet> selectAllDechets () {
+		return Modele.selectAllDechets();
+	}
+	public static ArrayList<Dechet> selectLikeDechets (String filtre) {
+		return Modele.selectLikeDechets(filtre);
+	}
+	public static void deleteDechet (int idDechet) {
+		Modele.deleteDechet(idDechet);
+	}
+	public static Dechet selectWhereDechet(int idDechet) {
+		return Modele.selectWhereDechet(idDechet);
+	}
+	public static void updateDechet (Dechet unDechet) {
+		Modele.updateDechet(unDechet);
+	}
+	
+	
+	/****** Gestion de la consommation d'eau ******/
+	
+	public static void insertConsommationEau (ConsommationEau uneConsommationEau) {
+		Modele.insertDechet(uneConsommationEau);
+	}
+	public static ArrayList<ConsommationEau> selectAllConsommationsEau () {
+		return Modele.selectAllConsommationsEau();
+	}
+	public static ArrayList<ConsommationEau> selectLikeConsommationsEau (String filtre) {
+		return Modele.selectLikeConsommationsEau(filtre);
+	}
+	public static void deleteConsommationEau (int idConsommationEau) {
+		Modele.deleteConsommationEau(idConsommationEau);
+	}
+	public static Dechet selectWhereConsommationEau(int idConsommationEau) {
+		return Modele.selectWhereConsommationEau(idConsommationEau);
+	}
+	public static void updateConsommationEau (ConsommationEau uneConsommationEau) {
+		Modele.updateConsommationEau(uneConsommationEau);
+	}
+
+	
+	/****** Gestion de l'empreinte carbone ******/
+	
+	public static void insertEmpreinteCarbone (EmpreinteCarbone uneEmpreinteCarbone) {
+		Modele.insertDechet(uneEmpreinteCarbone);
+	}
+	public static ArrayList<EmpreinteCarbone> selectAllEmpreintesCarbone () {
+		return Modele.selectAllEmpreintesCarbone();
+	}
+	public static ArrayList<EmpreinteCarbone> selectLikeEmpreintesCarbone (String filtre) {
+		return Modele.selectLikeEmpreintesCarbone(filtre);
+	}
+	public static void deleteEmpreinteCarbone (int idDechetEmpreinteCarbone) {
+		Modele.deleteEmpreinteCarbone(idDechetEmpreinteCarbone);
+	}
+	public static Dechet selectWhereEmpreinteCarbone(int idDechetEmpreinteCarbone) {
+		return Modele.selectWhereEmpreinteCarbone(idDechetEmpreinteCarbone);
+	}
+	public static void updateEmpreinteCarbone (EmpreinteCarbone uneEmpreinteCarbone) {
+		Modele.updateEmpreinteCarbone(uneEmpreinteCarbone);
+	}
+
 
 }
